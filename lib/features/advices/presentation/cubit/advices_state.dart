581183311b3908082f65af89b5eb9318599462c0
rodriguesjeff <jefferson.rodrigues@seventh.com.br>
2023-02-26 14:47:08 -0300

    initial commit

---
+++ lib/features/advices/presentation/cubit/advices_state.dart
@@ -0,0 +1,10 @@
+part of 'advices_cubit.dart';
+
+abstract class AdvicesState extends Equatable {
+  const AdvicesState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AdvicesInitial extends AdvicesState {}
